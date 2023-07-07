10ca1fbfddbf3d817ae403b94b181e2846b6dd7d
Etech consulting <99599657+etechDevops@users.noreply.github.com>
2023-07-07 19:30:13 -0400

    Update main.tf

--- target/systems/ec2/main.tf
+++ target/systems/ec2/main.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.7.0"
+    }
+  }
+}
 
+# provider "aws" {
+#   region = "us-west-2"
+# }
+
+module "server" {
+    source = "https://github.com/etechDevops/etech-team5-base-iac/tree/main//modules/ec2" 
+    num = 5
+    region = "us-east-1"
+    ami = ""
+}
